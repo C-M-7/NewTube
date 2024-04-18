7db21d275ae932d0654d3877adc84b01f15f60c7
C-M-7 <chiranm007@gmail.com>
2024-04-18 23:50:36 +0530

    added videos to feed.jsx

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -5,9 +5,10 @@ import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
 
   useEffect(()=>{
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data)=>setVideos(data.items));
   }, [selectedCategory]);
 
   return (
@@ -27,7 +28,7 @@ const Feed = () => {
           {selectedCategory} <span style={{color : '#F31503'}}>videos</span>
         </Typography>
 
-        <Videos videos={[]}/>
+        <Videos videos={videos}/>
       </Box> 
     </Stack>
   )
